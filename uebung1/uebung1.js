e7e904717d62ca1790d2a970d76427ccb9c5b1e4
shanthosh04 <shanthosh1610@gmail.com>
2024-06-17 19:17:31 +0200

    added uebung1

---
+++ uebung1/uebung1.js
@@ -0,0 +1,12 @@
+function replaceString(str) {
+  const replacedStr = str
+    .replace(/ä/g, "ae") // ä -> ae
+    .replace(/ö/g, "oe") // ö -> oe
+    .replace(/ü/g, "ue") // ü -> ue
+  
+  return replacedStr
+}
+
+const text = replaceString("Glücklich")
+const h1 = document.getElementById("title")
+h1.innerText = text
